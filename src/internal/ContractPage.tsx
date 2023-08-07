d3a0b5168eb7784679aec2c9a748ef2f77548eb5
KassCrypto <kasscrypto@gmail.com>
2023-08-07 19:21:00 +0100

    Added balance

--- src/internal/ContractPage.tsx
+++ src/internal/ContractPage.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Contract } from "ethers";
+import { Contract, formatEther } from "ethers";
 import ContractView from "@/internal/ContractView";
 import { Spinner, TextInput } from "flowbite-react";
-import { useProvider, useSigner } from "@/internal/hooks";
+import { useBalance, useProvider, useSigner } from "@/internal/hooks";
 
 const ContractPage = () => {
   const { address } = useParams();
@@ -12,6 +12,7 @@ const ContractPage = () => {
   const provider = useProvider();
   const [contract, setContract] = useState<Contract | null>(null);
   const [name, setName] = useState<string>("Contract");
+  const balance = useBalance(address || "0x0");
 
   useEffect(() => {
     const connector = signer || provider;
@@ -35,11 +36,15 @@ const ContractPage = () => {
 
   return (
     <div className="container mx-auto my-10 px-4">
-      <article className="format dark:text-white mx-auto">
+      <article className="format dark:text-white mx-auto mb-8">
         <h3 className="dark:text-white">Contract: {name}</h3>
         <TextInput className="my-4" value={address} readOnly />
-        <p>Interact with this contract below.</p>
+        <p className="text-sm">
+          Contract Balance: {formatEther(balance?.toString() || "0")}{" "}
+          <small>ETH</small>
+        </p>
       </article>
+      <hr />
       <div className="my-10 mx-auto max-w-2xl p-4 rounded">
         {contract ? (
           <ContractView contract={contract} name={name} />
